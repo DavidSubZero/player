c87d515f22b3c14379d0babc34de517f36e4ca23
DavidSubZero <36253589+DavidSubZero@users.noreply.github.com>
2018-03-22 17:15:23 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
-sudo apt-get install gnome-schedule
-sudo apt-get install curl
 cd /home;
-CUSER="subzero";
-echo $PWD;
-sudo cp -R /media/$CUSER/CRUZER/croninit.sh /home/;
 crontab -r;
 { crontab -l -u root; echo 'PATH=/home:/usr/local/bin:/bin/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'; } | crontab -u root -;
 { crontab -l -u root; echo '1 0 * * * sudo chmod -x /home/croninit.sh; sudo bash /home/croninit.sh'; } | crontab -u root -
