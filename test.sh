ada4b8de6cecad56ac19f5410d1000de77cbabe7
DavidSubZero <36253589+DavidSubZero@users.noreply.github.com>
2018-03-22 17:04:30 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ cd /home;
 CUSER="subzero";
 echo $PWD;
 sudo cp -R /media/$CUSER/CRUZER/croninit.sh /home/;
-crontab -r root -;
+crontab -r;
 { crontab -l -u root; echo 'PATH=/home:/usr/local/bin:/bin/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'; } | crontab -u root -;
 { crontab -l -u root; echo '1 0 * * * sudo chmod -x /home/croninit.sh; sudo bash /home/croninit.sh'; } | crontab -u root -
